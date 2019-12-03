3f7583d8f701ea0cabc2feee8bba5c2058f2fd72
sauxpa <46052865+sauxpa@users.noreply.github.com>
2019-12-03 15:46:15 +0100

    add kudos

--- bivariate_copula/base_copula.py
+++ bivariate_copula/base_copula.py
@@ -1,3 +1,6 @@
+# https://mediatum.ub.tum.de/doc/1119201/1119201.pdf for the explicit computation of 
+# various copula functions
+
 import numpy as np
 import abc
 from statsmodels.distributions.empirical_distribution import ECDF
