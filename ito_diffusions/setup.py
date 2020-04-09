16b62df58a85702e16e4d13e1a5ff46178a9ccbd
sauxpa <patrick.jr.saux@gmail.com>
2020-04-09 16:59:50 +0200

    add description

--- ito_diffusions/setup.py
+++ ito_diffusions/setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
 
-setup(name='ito_diffusions',
-      version='1.0.1',
-      install_requires=['numpy', 'scipy', 'pandas', 'mpmath']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ito_diffusions',
+    version='1.0.2',
+    author="Patrick Saux",
+    author_email="patrick.jr.saux@gmail.com",
+    description="Library for stochastic process simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sauxpa/stochastic",
+    install_requires=['numpy', 'scipy', 'pandas', 'mpmath'],
+    python_requires='>=3.6',
 )
