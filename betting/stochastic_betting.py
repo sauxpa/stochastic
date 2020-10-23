d4970e70b87fda2503ab8bcf491bce186ece3c6b
sauxpa <patrick.jr.saux@gmail.com>
2020-10-23 19:03:42 +0200

    add custom bet

--- betting/stochastic_betting.py
+++ betting/stochastic_betting.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from typing import Type
+from typing import Type, Callable
 import abc
 
 
@@ -16,6 +16,12 @@ class Coin():
     def p(self, new_p: float):
         self._p = new_p
 
+    @property
+    def entropy(self) -> float:
+        return -(
+            self.p * np.log(self.p) + (1 - self.p) * np.log(1 - self.p)
+        )
+
     def simulate(self, N: int) -> pd.Series:
         return pd.Series(data=np.random.rand(N) < self.p)
 
@@ -23,14 +29,19 @@ class Coin():
 class Bet(abc.ABC):
     """Generic class for bet on the results of generating
     random outcomes of coin.
+    Seed parameter is used to consistently recreate the same
+    sequence of coin tosses when different Bet subclasses
+    use the same seed.
     """
     def __init__(
         self,
         coin: Type[Coin],
-        init_fortune: float = 100.0
+        init_fortune: float = 100.0,
+        seed: float = 0,
     ):
         self._coin = coin
         self._init_fortune = init_fortune
+        self._seed = seed
 
     @property
     def coin(self) -> Type[Coin]:
@@ -48,6 +59,14 @@ class Bet(abc.ABC):
     def init_fortune(self, new_init_fortune: float):
         self._init_fortune = new_init_fortune
 
+    @property
+    def seed(self) -> float:
+        return self._seed
+
+    @seed.setter
+    def seed(self, new_seed: float):
+        self._seed = new_seed
+
     @abc.abstractmethod
     def strat(self, results: pd.Series) -> pd.DataFrame:
         """To be instanciated in the children classes.
@@ -56,6 +75,7 @@ class Bet(abc.ABC):
         pass
 
     def simulate(self, N: int) -> pd.DataFrame:
+        np.random.seed(self.seed)
         outcomes = self.coin.simulate(N)
         df = self.strat(outcomes)
         df['gains'] = (df['outcomes'] == df['bet']) * df['size'] \
@@ -76,11 +96,16 @@ class RandomBet(Bet):
         init_fortune: float = 100.0,
         unit_bet: float = 1.0,
         bet_style: str = 'fractional',
+        seed: float = 0,
     ):
         self._p = p
         self._unit_bet = unit_bet
         self._bet_style = bet_style
-        super().__init__(coin=coin, init_fortune=init_fortune)
+        super().__init__(
+            coin=coin,
+            init_fortune=init_fortune,
+            seed=seed,
+        )
 
     @property
     def p(self) -> float:
@@ -142,22 +167,24 @@ class RandomBet(Bet):
         return df
 
 
-class ExactBellmanBet(Bet):
-    """Bet sizing by maximization of a log utility with known probability p.
-    If p is indeed the probability to win a single occurrence, this strategy is
-    optimal (in terms of log-utility by definition), but if p is mispecified
-    (for example if a coin toss wins with 60% probability and the Bellman
-    criterion is used with p=80%) this strategy can fail and lead to
-    huge losses.
+class ExactArbitraryBet(Bet):
+    """User-defined fractional rule.
     """
     def __init__(
         self,
         coin: Type[Coin],
         p: float = 0.5,
         init_fortune: float = 100.0,
+        rule: Callable = None,
+        seed: float = 0,
     ):
         self._p = p
-        super().__init__(coin=coin, init_fortune=init_fortune)
+        self._rule = rule
+        super().__init__(
+            coin=coin,
+            init_fortune=init_fortune,
+            seed=seed,
+        )
 
     @property
     def p(self) -> float:
@@ -167,6 +194,14 @@ class ExactBellmanBet(Bet):
     def p(self, new_p: float):
         self._p = new_p
 
+    @property
+    def rule(self) -> Callable:
+        return self._rule
+
+    @rule.setter
+    def rule(self, new_rule: Callable):
+        self._rule = new_rule
+
     def strat(self, outcomes: pd.Series) -> pd.DataFrame:
         df = pd.DataFrame(data=outcomes, columns=['outcomes'])
 
@@ -175,8 +210,8 @@ class ExactBellmanBet(Bet):
 
         sizes = np.empty(len(outcomes))
 
-        # Bet a constant fraction of total fortune proportional
-        fraction = 2 * self.p - 1 if which_side else 2 * (1 - self.p) - 1
+        # Custom fractional bet
+        fraction = self.rule(self.p) if which_side else self.rule(1 - self.p)
 
         fortune = self.init_fortune
 
@@ -190,26 +225,33 @@ class ExactBellmanBet(Bet):
         return df
 
 
-class EmpiricalBellmanBet(Bet):
-    """Same as exact Bellman but the parameter p is estimated on-the-fly
+class EmpiricalArbitraryBet(Bet):
+    """Same as ExactArbitrary but the parameter p is estimated on-the-fly
     rather than known in advance, much like in a bandit context.
     """
     def __init__(
         self,
         coin: Type[Coin],
         init_fortune: float = 100.0,
+        rule: Callable = None,
         warmup_time: int = 1,
+        seed: float = 0,
     ):
+        self._rule = rule
         self._warmup_time = warmup_time
-        super().__init__(coin=coin, init_fortune=init_fortune)
-
+        super().__init__(
+            coin=coin,
+            init_fortune=init_fortune,
+            seed=seed,
+        )
+        
     @property
-    def p(self) -> float:
-        return self._p
+    def rule(self) -> Callable:
+        return self._rule
 
-    @p.setter
-    def p(self, new_p: float):
-        self._p = new_p
+    @rule.setter
+    def rule(self, new_rule: Callable):
+        self._rule = new_rule
 
     @property
     def warmup_time(self) -> int:
@@ -233,8 +275,8 @@ class EmpiricalBellmanBet(Bet):
                 p = outcomes.iloc[:t].mean()
             # Which side seems biased?
             which_side = p >= 0.5
-            # Bet a fraction of total fortune proportional
-            fraction = 2 * p - 1 if which_side else 2 * (1 - p) - 1
+            # Custom fractional bet
+            fraction = self.rule(p) if which_side else self.rule(1 - p)
 
             sizes[t] = fraction * fortune
             fortune *= 1 + fraction if which_side == outcome else 1 - fraction
@@ -243,3 +285,47 @@ class EmpiricalBellmanBet(Bet):
         df['size'] = sizes
 
         return df
+
+
+class ExactBellmanBet(ExactArbitraryBet):
+    """Bet sizing by maximization of a log utility with known probability p.
+    If p is indeed the probability to win a single occurrence, this strategy is
+    optimal (in terms of log-utility by definition), but if p is mispecified
+    (for example if a coin toss wins with 60% probability and the Bellman
+    criterion is used with p=80%) this strategy can fail and lead to
+    huge losses.
+    """
+    def __init__(
+        self,
+        coin: Type[Coin],
+        p: float = 0.5,
+        init_fortune: float = 100.0,
+        seed: float = 0,
+    ):
+        super().__init__(
+            coin=coin,
+            p=p,
+            init_fortune=init_fortune,
+            rule=lambda p: 2 * p - 1,
+            seed=seed,
+        )
+
+
+class EmpiricalBellmanBet(EmpiricalArbitraryBet):
+    """Same as exact Bellman but the parameter p is estimated on-the-fly
+    rather than known in advance, much like in a bandit context.
+    """
+    def __init__(
+        self,
+        coin: Type[Coin],
+        init_fortune: float = 100.0,
+        warmup_time: int = 1,
+        seed: float = 0,
+    ):
+        super().__init__(
+            coin=coin,
+            init_fortune=init_fortune,
+            rule=lambda p: 2 * p - 1,
+            warmup_time=warmup_time,
+            seed=seed,
+        )
